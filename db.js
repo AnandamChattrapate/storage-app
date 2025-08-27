e5e349a1019cb89173574892f567cb02219cacc5
Anandam Chattrapate <23eg105e05@anurag.edu.in>
2025-08-27 14:48:55 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
 const mysql = require('mysql2');
 
+// railway database credentials
 const connection = mysql.createConnection({
-  host: 'ballast.proxy.rlwy.net',
-  port: 31834,
-  user: 'root',
-  password: 'dVqTsJbWKXENVwopxLzoMFtJunkbsnCD',
-  database: 'railway'
+  host: "ballast.proxy.rlwy.net",
+  user: "root",
+  password: "dVqTsJbWKXENVwopxLzoMFtJunkbsnCD",
+  database: "railway",
+  port: 31834
 });
 
+// connect
 connection.connect((err) => {
   if (err) {
-    console.error('DB connection failed:', err);
+    console.error('Error connecting: ' + err.stack);
     return;
   }
-  console.log('Connected to Railway MySQL!');
+  console.log('✅ Connected to Railway DB as id ' + connection.threadId);
 });
 
 module.exports = connection;
